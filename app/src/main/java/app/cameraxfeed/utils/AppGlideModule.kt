1c3f55093a71fabf7a0691ce7917343fcb47bd79
Adam Hurwitz <3938076+AdamSHurwitz@users.noreply.github.com>
2020-08-13 19:02:01 -0700

    init: CameraXFeed

---
+++ app/src/main/java/app/cameraxfeed/utils/AppGlideModule.kt
@@ -0,0 +1,8 @@
+package app.adamhurwitz.cameraxfeed.utils
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+
+@GlideModule
+class MyAppGlideModule : AppGlideModule()
